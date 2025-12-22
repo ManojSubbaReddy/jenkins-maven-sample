77ed7719e017a00d6cc4a16242dc9549ed79c44e
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2025-12-22 11:11:02 +0530

    Fix greeting message formatting in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java Maven Jenkins Sample Github!");
+        System.out.println("Hello from Java Maven Jenkins Sample Github !");
     }
 
     public static String greet() {
